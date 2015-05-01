afea9ef72805e7880506b2bc2d1a0c548de8beb9
Leon <student@epicodus.com>
2015-05-01 09:36:34 -0700

    Return the id of the word.

--- lib/word.rb
+++ lib/word.rb
@@ -1,10 +1,16 @@
 class Word
+  @@word_list =[]
 
   define_method(:initialize) do |words|
     @words = words
+    @id = @@word_list.length().+(1)
   end
 
   define_method(:words) do
     @words
+  end
+
+  define_method(:id) do
+    @id
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -8,4 +8,11 @@ require('word')
         expect(test_words.words()).to(eq('cat'))
       end
     end
+
+    describe('#id') do
+      it("returns the id of the word") do
+        test_words = Word.new('cat')
+        expect(test_words.id()).to(eq(1))
+      end
+    end
   end
